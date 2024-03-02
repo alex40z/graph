208f9a4357662acc9eb90896b788af100b710364
Александр Зеленов <zelenov.a@vector-kaluga.ru>
2024-03-02 18:30:24 +0300

    lesson-10

---
+++ graph.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+print(data)
+data = pd.get_dummies(data['whoAmI'])
+print(data)
